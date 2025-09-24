7d01f676796020184d0e10c19310cea80d457951
Asadbek Meyliyev <asadcitymeyliyev@gmail.com>
2025-09-24 16:53:28 +0500

    Initial PWA commit

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,11 @@
 self.addEventListener("install", (event) => {
   console.log("Service Worker установлен");
+  self.skipWaiting();
 });
-
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker активирован");
+  return self.clients.claim();
+});
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(fetch(event.request).catch(() => caches.match('/index.html')));
 });
